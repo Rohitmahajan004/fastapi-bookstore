b66deec2a70171b08ac46179fcebd2e47377043a
Rohit Mahajan <139931027+Rohitmahajan004@users.noreply.github.com>
2025-10-28 19:31:05 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Example: postgresql://username:password@localhost:5432/bookstore
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
