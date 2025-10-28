b6f83c0bd5741a32b6f569d21e53f15aef5e6c21
Rohit Mahajan <139931027+Rohitmahajan004@users.noreply.github.com>
2025-10-28 19:33:03 +0530

    Rename table names for authors and books

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 class Author(Base):
-    __tablename__ = "authors"
+    __tablename__ = "author"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
@@ -12,11 +12,11 @@ class Author(Base):
     books = relationship("Book", back_populates="author", cascade="all, delete")
 
 class Book(Base):
-    __tablename__ = "books"
+    __tablename__ = "book"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     genre = Column(String)
     author_id = Column(Integer, ForeignKey("authors.id", ondelete="CASCADE"), nullable=False)
 
-    author = relationship("Author", back_populates="books")
+    author = relationship("Author", back_populates="book")
